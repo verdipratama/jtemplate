1ac38f1f51a0dfa9e9905808891de66bcac3e815
verdipratama <verdipratama@icloud.com>
2022-10-25 23:50:14 +0700

    Initial commits

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -34,5 +34,3 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
-
-console.log("Hello World");
